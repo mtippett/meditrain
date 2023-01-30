70cdd5641495875b3027ea8795047e884f1a1069
Matthew Tippett <tippettm@gmail.com>
2023-01-30 13:08:13 -0800

    Generate averaged periodogram for band power calculation and push up to App

--- src/DeviceControl.jsx
+++ src/DeviceControl.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react';
 import EEGChannels from './EEGChannels';
 import MuseData from './MuseData';
+import { fft, util as fftUtil } from "fft-js"
 
 
-function DeviceControl() {
+function DeviceControl({onPeriodgramUpdated}) {
 
     const eegChannelData = useRef([]);
     const channelMaps = useRef([]);
@@ -11,27 +12,44 @@ function DeviceControl() {
     const [updater, setUpdater] = useState(0);
     const [viewEEG, setViewEEG] = useState(false)
     const eegInterval = useRef(0);
+    const powerInterval = useRef(0);
+
 
     useEffect(
         () => {
-            if (viewEEG) {
-                eegInterval.current =
-                    setInterval(() => {
-                        setUpdater(updater + 1);
-                    }, 100);
+            if (powerInterval.current === 0 && channelMaps.current.length !== 0) {
 
-                return function cleanup() {
-                    clearInterval(eegInterval.current);
-                };
+                powerInterval.current = setInterval(() => {
+                    eegChannelData.current.forEach(electrode => {
+                        if (electrode.samples.length > 1024) {
+                            electrode.periodograms.push(calcPeriodogram(electrode.samples.slice(-1024)));
+                            electrode.periodograms = electrode.periodograms.slice(-4);
+                            electrode.averagedPeriodograms = averagePeriodogram(electrode.periodograms);
+
+                            onPeriodgramUpdated(eegChannelData.current);
+                        }
+                    });
+                }, 1000);
             }
+            if (viewEEG) {
+                eegInterval.current = setInterval(() => {
+                    setUpdater(updater + 1);
+                }, 100);
+            }
+            return function cleanup() {
+                clearInterval(eegInterval.current);
+            };
+
         },
-        [updater,viewEEG]
+        [updater, viewEEG]
     );
 
     function updateChannelMaps(maps) {
         if (channelMaps.current.length === 0) {
             console.log("setting maps", channelMaps, [...maps])
             channelMaps.current = maps;
+
+            setUpdater(updater + 1);
         }
     }
 
@@ -43,13 +61,55 @@ function DeviceControl() {
             {
                 electrode: data.electrode,
                 location: channelMaps.current[data.electrode],
-                samples: []
+                samples: [],
+                periodograms: []
             }
         }
 
-        eegChannelData.current[data.electrode].samples.push(...data.samples);
+        const samples = eegChannelData.current[data.electrode].samples
+        samples.push(...data.samples);
+        eegChannelData.current[data.electrode].samples = samples.slice(-4096)
 
+    }
 
+    function averagePeriodogram(periodograms) {
+        const averagedPeriodogram = {frequencies: periodograms[0].frequencies, magnitudes: []}
+        const numPeriodograms = periodograms.length;
+
+        // Use periodogram[0], as a template and average across all 
+        periodograms[0].magnitudes.forEach((element, index) => {
+            let sum = 0;
+            for (let i = 0; i < numPeriodograms; i++) {
+                sum += periodograms[i].magnitudes[index];
+            }
+            averagedPeriodogram.magnitudes[index] = sum/numPeriodograms;
+        });
+
+        // console.log("av", averagedPeriodogram);
+        return averagedPeriodogram;
+        
+    }
+
+    function calcPeriodogram(eegData) {
+
+        // Define the sampling rate and frequency bands
+        const samplingRate = 256;
+        const lowerFrequency = 8;
+        const upperFrequency = 12;
+
+        // Filter the data to remove unwanted noise
+        // const filteredData = filterData(eegData);
+        const filteredData = eegData;
+
+        // Perform the FFT on the filtered data
+        const phasors = fft(eegData);
+
+        const frequencies = fftUtil.fftFreq(phasors, samplingRate);
+        const magnitudes = fftUtil.fftMag(phasors);
+
+        var both = { frequencies: frequencies, magnitudes: magnitudes }
+
+        return both;
     }
 
     return (
@@ -59,7 +119,9 @@ function DeviceControl() {
             </div>
             <div>
                 <MuseData onNewData={onNewData} updateChannelMaps={updateChannelMaps} />
-                <button onClick={() => setViewEEG(!viewEEG)}>View EEG Data</button> 
+                {channelMaps.current.length !== 0 && 
+                    <button onClick={() => setViewEEG(!viewEEG)}>View EEG Data</button>
+                }
                 {viewEEG &&
                     <EEGChannels eegChannelData={eegChannelData.current} />
                 }
--- src/EEGChannel.js
+++ src/EEGChannel.js
@@ -1,10 +1,23 @@
+import React, { useState } from 'react';
 import EEGTrace from './EEGTrace'
-function EEGChannel({ channel}) {
-    
+import EEGPeriodogram from './EEGPeriodogram'
+
+function EEGChannel({ channel }) {
+    const [eegView, setEEGView] = useState(false);
+    const [powerView, setPowerView] = useState(false);
+
     return (
         <div id={channel.electrode}>
-        {channel.location} {channel.electrode} {channel.samples.length} <EEGTrace samples={channel.samples.slice(-500)} />
-
+            {channel.location} {channel.electrode} {channel.samples.length}
+            <button onClick={() => { console.log("Flipping EEGView"); setEEGView(!eegView) }}>Toggle EEG</button>
+            {eegView &&
+                <EEGTrace samples={channel.samples} />}
+            {channel.periodograms.length > 0 && 
+                    <button onClick={() => { console.log("Flipping PeriodogramView"); setPowerView(!powerView) }}>Toggle Periodograms</button> }
+            { powerView &&
+            
+                    <EEGPeriodogram periodograms={channel.periodograms} averagedPeriodogram={channel.averagedPeriodograms}/>
+            }
         </div>
     );
 }
